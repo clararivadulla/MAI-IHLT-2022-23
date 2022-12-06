10a24e07702968599b8c4376415cd161e446aa96
Clara Rivadulla Duró <clararivadulladuro@gmail.com>
2022-12-06 23:33:01 +0100

    Update feature_extractor.py

--- STS/code_/feature_extractor.py
+++ STS/code_/feature_extractor.py
@@ -29,19 +29,40 @@ tags = {'NN': wordnet.NOUN,
 
 wnl = WordNetLemmatizer()
 
-def tokenize(sentences):
+def tokenize(sentences, sw=False):
     sentences_tokens = []
     sentences_pairs = []
     for sentence in sentences:
         tokens = nltk.word_tokenize(sentence)  # tokenize
         pairs = nltk.pos_tag(tokens)  # get the pos of the tokens
-        tokens = [regex.sub('', w.lower()) for w in tokens if
-                  not w.lower() in stopw]  # remove stopwords and symbols from each word
+        if sw:
+            tokens = [regex.sub('', w.lower()) for w in tokens]
+        else:
+            tokens = [regex.sub('', w.lower()) for w in tokens if
+                      not w.lower() in stopw]  # remove stopwords and symbols from each word
         tokens = [w for w in tokens if w]  # remove empty elements
         sentences_tokens.append(set(tokens))
         sentences_pairs.append(pairs)
     return sentences_tokens, sentences_pairs
 
+def lemmatize(sentences_POS, sw=False):
+    lemmas = []
+    for sentence in sentences_POS:
+        lemmatized = [pos_wn(pair) for pair in sentence]
+        if sw:
+            lemmatized = [regex.sub('', w.lower()) for w in lemmatized]
+        else:
+            lemmatized = [regex.sub('', w.lower()) for w in lemmatized if
+                          not w.lower() in stopw]  # remove stopwords and symbols from each word
+        lemmatized = [w for w in lemmatized if w]  # remove empty elements
+        lemmas.append(set(lemmatized))
+    return lemmas
+
+def jaccard_empty(set1, set2):
+    if len(set1) != 0 and len(set2) != 0:
+        return jaccard_distance(set1, set2)
+    else:
+        return 0
 
 def pos_wn(pair):
     word = pair[0].lower()
@@ -68,34 +89,6 @@ def longest_common_subsequence(t1, t2):
 
     return dp[len(tokens2)][len(tokens1)] / min(len(tokens1), len(tokens2))
 
-def num_verbs_diff(pos1, pos2):
-    v1 = len([v for v in pos1 if v[1].startswith('V')])
-    v2 = len([v for v in pos2 if v[1].startswith('V')])
-    if v1 == 0 and v2 == 0:
-        return 0
-    return abs(v1 - v2) / (v1 + v2)
-
-def num_nouns_diff(pos1, pos2):
-    n1 = len([n for n in pos1 if n[1].startswith('N')])
-    n2 = len([n for n in pos2 if n[1].startswith('N')])
-    if n1 == 0 and n2 == 0:
-        return 0
-    return abs(n1 - n2) / (n1 + n2)
-
-def num_adjs_diff(pos1, pos2):
-    a1 = len([a for a in pos1 if a[1].startswith('J')])
-    a2 = len([a for a in pos2 if a[1].startswith('J')])
-    if a1 == 0 and a2 == 0:
-        return 0
-    return abs(a1 - a2) / (a1 + a2)
-
-def num_advs_diff(pos1, pos2):
-    adv1 = len([a for a in pos1 if a[1].startswith('R')])
-    adv2 = len([a for a in pos2 if a[1].startswith('R')])
-    if adv1 == 0 and adv2 == 0:
-        return 0
-    return abs(adv1 - adv2) / (adv1 + adv2)
-
 def longest_common_substring(t1, t2):
 
     s = ''.join(t1)
@@ -116,18 +109,6 @@ def longest_common_substring(t1, t2):
                 dp[i % 2][j] = 0
     return res / min(m, n)
 
-
-def lemmatize(sentences_POS):
-    lemmas = []
-    for sentence in sentences_POS:
-        lemmatized = [pos_wn(pair) for pair in sentence]
-        lemmatized = [regex.sub('', w.lower()) for w in lemmatized if
-                      not w.lower() in stopw]  # remove stopwords and symbols from each word
-        lemmatized = [w for w in lemmatized if w]  # remove empty elements
-        lemmas.append(set(lemmatized))
-    return lemmas
-
-
 def lesk(sentences, POS):
     syns = []
     for sentence, pairs in zip(sentences, POS):
@@ -137,8 +118,7 @@ def lesk(sentences, POS):
         syns.append(set(synsets))
     return syns
 
-
-def syntactic_role_sim(POS1, POS2):
+def syntactic_role_sim(POS1, POS2, method='lin'):
     similarities = []
     for pairs1, pairs2 in zip(POS1, POS2):
         syn1 = []
@@ -151,21 +131,22 @@ def syntactic_role_sim(POS1, POS2):
             tag = tags.get(pair[1])
             if tag and pair[0].lower() not in stopw and pair[0].lower() in nltk.corpus.words.words():
                 syn2.append((pair, (wordnet.synsets(pair[0], tag))))
-        lin_sum = 0
+        sum = 0
         for s1, s2 in itertools.product(syn1, syn2):
-            print(s1, s2)
-            if s1[0][1] == s2[0][1]:
-                lin_sum += s1[1][0].lin_similarity(s2[1][0], semcor_ic)
-        print(lin_sum)
-
-        similarities.append((len(syn1) + len(syn2)) / lin_sum)
+            if s1[0][1] == s2[0][1] and len(s1[1]) > 0 and len(s2[1]) > 0 and method == 'path':
+                sum += s1[1][0].path_similarity(s2[1][0])
+            elif s1[0][1] == s2[0][1] and len(s1[1]) > 0 and len(s2[1]) > 0 and method == 'lch':
+                sum += s1[1][0].lch_similarity(s2[1][0])
+            elif s1[0][1] == s2[0][1] and len(s1[1]) > 0 and len(s2[1]) > 0 and method == 'wup':
+                sum += s1[1][0].wup_similarity(s2[1][0])
+            elif s1[0][1] == s2[0][1] and len(s1[1]) > 0 and len(s2[1]) > 0 and method == 'lin':
+                if s1[1][0].pos == s2[1][0].pos and s1[1][0].pos in {'n', 'v', 'r', 'a'}:
+                    sum += s1[1][0].lin_similarity(s2[1][0])
+        if sum != 0:
+            similarities.append((len(syn1) + len(syn2)) / sum)
+        else:
+            similarities.append(0)
     return similarities
-
-def jaccard_empty(set1, set2):
-    if len(set1) != 0 and len(set2) != 0:
-        return jaccard_distance(set1, set2)
-    else:
-        return 0
 
 def levenshtein_distance(s1, s2):
     return nltk.edit_distance(s1, s2) / max(len(s1), len(s2))
@@ -179,20 +160,26 @@ class Features:
 
         self.tokens1, self.pos1 = tokenize(self.pair1)
         self.tokens2, self.pos2 = tokenize(self.pair2)
+        self.tokens1_sw, _ = tokenize(self.pair1, sw=True) # with stopwords
+        self.tokens2_sw, _ = tokenize(self.pair2, sw=True) # with stopwords
 
         self.lemmas1 = lemmatize(self.pos1)
         self.lemmas2 = lemmatize(self.pos2)
+        self.lemmas1_sw = lemmatize(self.pos1, sw=True)
+        self.lemmas2_sw = lemmatize(self.pos2, sw=True)
 
-    def simple_tokens(self):
-        self.jac_simp = [(1 - jaccard_distance(t[0], t[1])) for t in zip(self.tokens1, self.tokens2)]
+    def tokens(self):
+        self.jac_tokens = [(1 - jaccard_distance(t[0], t[1])) for t in zip(self.tokens1, self.tokens2)]
+        self.jac_tokens_sw = [(1 - jaccard_distance(t[0], t[1])) for t in zip(self.tokens1_sw, self.tokens2_sw)]
 
-    def lemmatizer(self):
+    def lemmas(self):
         self.jac_lemmas = [(1 - jaccard_distance(t[0], t[1])) for t in zip(self.lemmas1, self.lemmas2)]
+        self.jac_lemmas_sw = [(1 - jaccard_distance(t[0], t[1])) for t in zip(self.lemmas1_sw, self.lemmas2_sw)]
 
     def lesk(self):
         l1 = lesk(self.pair1, self.pos1)
         l2 = lesk(self.pair2, self.pos2)
-        self.jac_lesk = [5 * (1 - jaccard_empty(t[0], t[1])) for t in zip(l1, l2)]
+        self.jac_lesk = [1 - jaccard_empty(t[0], t[1]) for t in zip(l1, l2)]
 
     def n_grams(self):
         self.jac_2grams = [(1 - jaccard_empty(set([g for g in nltk.ngrams(t[0], 2)]), set([g for g in nltk.ngrams(t[1], 2)]))) for
@@ -201,54 +188,66 @@ class Features:
                            t in zip(self.tokens1, self.tokens2)]
         self.jac_4grams = [(1 - jaccard_empty(set([g for g in nltk.ngrams(t[0], 4)]), set([g for g in nltk.ngrams(t[1], 4)]))) for
                            t in zip(self.tokens1, self.tokens2)]
-        self.jac_2grams_l = [
-            (1 - jaccard_empty(set([g for g in nltk.ngrams(l[0], 2)]), set([g for g in nltk.ngrams(l[1], 2)]))) for
-            l in zip(self.lemmas1, self.lemmas2)]
-        self.jac_3grams_l = [
-            (1 - jaccard_empty(set([g for g in nltk.ngrams(l[0], 3)]), set([g for g in nltk.ngrams(l[1], 3)]))) for
-            l in zip(self.lemmas1, self.lemmas2)]
-        self.jac_4grams_l = [
-            (1 - jaccard_empty(set([g for g in nltk.ngrams(l[0], 4)]), set([g for g in nltk.ngrams(l[1], 4)]))) for
-            l in zip(self.lemmas1, self.lemmas2)]
+        self.jac_2grams_sw = [
+            (1 - jaccard_empty(set([g for g in nltk.ngrams(t[0], 2)]), set([g for g in nltk.ngrams(t[1], 2)]))) for
+            t in zip(self.tokens1_sw, self.tokens2_sw)]
+        self.jac_3grams_sw = [
+            (1 - jaccard_empty(set([g for g in nltk.ngrams(t[0], 3)]), set([g for g in nltk.ngrams(t[1], 3)]))) for
+            t in zip(self.tokens1_sw, self.tokens2_sw)]
+        self.jac_4grams_sw = [
+            (1 - jaccard_empty(set([g for g in nltk.ngrams(t[0], 4)]), set([g for g in nltk.ngrams(t[1], 4)]))) for
+            t in zip(self.tokens1_sw, self.tokens2_sw)]
 
     def syntatic_role(self):
-        self.synt = syntactic_role_sim(self.pos1, self.pos2)
+        self.path_sim = syntactic_role_sim(self.pos1, self.pos2, method='path')
+        self.lin_sim = syntactic_role_sim(self.pos1, self.pos2, method='lin')
+        self.wup_sim = syntactic_role_sim(self.pos1, self.pos2, method='wup')
+        self.lch_sim = syntactic_role_sim(self.pos1, self.pos2, method='lch')
 
-    def lcs_sequence(self):
-        self.lcs_sequence_score = [longest_common_subsequence(t[0], t[1]) for t in
+    def lcs_subsequence(self):
+        self.lcs_subsequence = [longest_common_subsequence(t[0], t[1]) for t in
                                              zip(self.tokens1, self.tokens2)]
+        self.lcs_subsequence_sw = [longest_common_subsequence(t[0], t[1]) for t in
+                                zip(self.tokens1_sw, self.tokens2_sw)]
 
-    def lcs_string(self):
-        self.lcs_string_score = [longest_common_substring(t[0], t[1]) for t in
+    def lcs_substring(self):
+        self.lcs_substring = [longest_common_substring(t[0], t[1]) for t in
                                            zip(self.tokens1, self.tokens2)]
+        self.lcs_substring_sw = [longest_common_substring(t[0], t[1]) for t in
+                              zip(self.tokens1_sw, self.tokens2_sw)]
     def levenshtein_dist(self):
         self.leven_dist = [levenshtein_distance(p[0], p[1]) for p in
                                              zip(self.pair1, self.pair2)]
-    def num_tags(self):
-        self.verbs_diff = [num_verbs_diff(p[0], p[1]) for p in
-                                             zip(self.pos1, self.pos2)]
-        self.nouns_diff = [num_nouns_diff(p[0], p[1]) for p in
-                           zip(self.pos1, self.pos2)]
-        self.adjs_diff = [num_adjs_diff(p[0], p[1]) for p in
-                           zip(self.pos1, self.pos2)]
-        self.advs_diff = [num_advs_diff(p[0], p[1]) for p in
-                          zip(self.pos1, self.pos2)]
 
     def extract_all(self):
-        # self.syntatic_role()
-        self.simple_tokens()
-        self.lemmatizer()
-        self.lesk()
+        self.tokens()
+        self.lemmas()
         self.n_grams()
-        self.lcs_sequence()
-        self.lcs_string()
+        self.lcs_substring()
+        self.lcs_subsequence()
+        self.lesk()
+        self.syntatic_role()
         self.levenshtein_dist()
-        self.num_tags()
 
         return pd.DataFrame(
-            {'Jaccard Tokens': self.jac_simp, 'Jaccard Lemmas': self.jac_lemmas, 'Lesk Similarity': self.jac_lesk, '2-grams Tokens': self.jac_2grams,
-             '3-grams Tokens': self.jac_3grams, '4-grams Tokens': self.jac_4grams, '2-grams Lemmas': self.jac_2grams_l,
-             '3-grams Lemmas': self.jac_3grams_l, '4-grams Lemmas': self.jac_4grams_l,
-             'Longest Common Subsequence': self.lcs_sequence_score, 'Longest Common Substring': self.lcs_string_score,
-             'Levenshtein Distance': self.leven_dist, 'Verb Tags': self.verbs_diff, 'Noun Tags': self.nouns_diff,
-             'Adjective Tags': self.adjs_diff, 'Adverb Tags': self.advs_diff})
+            {'Tokens Jac. Sim.': self.jac_tokens,
+             'Tokens (stop-words) Jac. Sim.': self.jac_tokens_sw,
+             'Lemmas Jac. Sim.': self.jac_lemmas,
+             'Lemmas (stop-words) Jac. Sim.': self.jac_lemmas_sw,
+             'Bigrams Jac. Sim.': self.jac_2grams,
+             'Bigrams (stop-words) Jac. Sim.': self.jac_2grams_sw,
+             'Trigrams Jac. Sim.': self.jac_3grams,
+             'Trigrams (stop-words) Jac. Sim.': self.jac_3grams_sw,
+             'Fourgrams Jac. Sim.': self.jac_4grams,
+             'Fourgrams (stop-words) Jac. Sim.': self.jac_4grams_sw,
+             'Longest Common Subsequence': self.lcs_subsequence,
+             'Longest Common Subsequence (stop-words)': self.lcs_subsequence_sw,
+             'Longest Common Substring': self.lcs_substring,
+             'Longest Common Substring (stop-words)': self.lcs_substring_sw,
+             'Lesk Jac. Sim.': self.jac_lesk,
+             'Leacock-Chodorow Sim.': self.lch_sim,
+             'Path Sim.': self.path_sim,
+             'Wu-Palmer Sim.': self.wup_sim,
+             'Lin Sim.': self.lin_sim,
+             'Levenshtein Distance': self.leven_dist,
+             })
